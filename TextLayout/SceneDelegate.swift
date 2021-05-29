835c3d98cbc92369c4e30f006372edd2999f544a
Lvv.me <cntrump@gmail.com>
2021-05-29 17:18:41 +0800

    init.

---
+++ TextLayout/SceneDelegate.swift
@@ -0,0 +1,13 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+The scene delegate class that Xcode generates.
+*/
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+}
+
